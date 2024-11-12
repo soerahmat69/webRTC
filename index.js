cc54121ddcc9a08f6d6572a1fdda77d02b9472fe
soerahmat69 <surahmatr38@gmail.com>
2024-11-12 13:41:34 +0800

    add:

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Menyajikan file di folder 'public'
 app.use(express.static('public'));
 
-// Setup signaling dengan Socket.IO
 io.on('connection', (socket) => {
   socket.on('offer', (offer) => {
     socket.broadcast.emit('offer', offer);
@@ -22,7 +20,6 @@ io.on('connection', (socket) => {
   });
 });
 
-// Memulai server pada port 5000
 server.listen(5000, () => {
   console.log('Server is running on http://localhost:5000');
 });
